f2adea3e084a54c683e150119c328b33bfaf27b7
Ivan Srbulov <ivan@markov.money>
2025-05-15 09:55:40 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Introduction
+This is a bash script to automate running Open WebUI on Linux systems with Cloudflare. The script currently works on Debian 12 with an AMD Radeon 7900XTX -- YMMV with other setups.
+
+# Setup
+1. Clone the repo
+2. Rename `exampleenv` to `.env` and set the variables to your own Cloudflare token and local IP address.
+3. If using an NVIDIA GPU, make the necessary changes to `run.sh`
+4. Navigate to the folder in terminal and run `chmod +x run.sh`
+5. Enter `/.run.sh`
+6. Optional: delete the .git
---
+++ run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+source .env
+
+# Step 0: Stop and remove any existing Docker images
+docker stop open-webui cloudflared watchtower ollama
+sleep 2 # Waits 2 seconds
+docker rm open-webui cloudflared watchtower ollama
+sleep 2 # Waits 2 seconds
+
+# Step 1: Create the custom Docker network NB: May throw an error if it already exists, it's fine if it does
+docker network create docker_bridge
+sleep 2  # Waits 2 seconds.
+
+# Step 2: Run the Ollama Container
+docker run -d \
+  --network docker_bridge \
+  -e OLLAMA_BASE_URL=http://host.docker.internal \
+  --restart always \
+  --device /dev/kfd --device /dev/dri \ # replace with --gpus=all if you have an NVIDIA GPU.
+  -v ollama:/root/.ollama \
+  -p 11434:11434 \
+  --name ollama ollama/ollama:rocm # replace with --name ollama ollama/ollama:main if you have an NVIDIA GPU. NB ROCm only works for compatible AMD Radeon GPUs.
+
+# Step 3: Run the WebUI container
+docker run -d \
+    -p 3000:8080 \
+    --network docker_bridge \
+    -v open-webui:/app/backend/data \
+    --name open-webui \
+    --restart always ghcr.io/open-webui/open-webui:main
+sleep 2  # Waits 2 seconds.
+
+# Step 4: Run the Cloudflare container (replace <CLOUDFLARED_KEY> with your actual token)
+docker run -d \
+    --network docker_bridge \
+    --name cloudflared \
+    cloudflare/cloudflared:latest tunnel --no-autoupdate run --token CLOUDFLARED_KEY --url=LOCAL_IP
+sleep 2  # Waits 2 seconds.
+
+# Step 5: Add Watchtower for automatic updates
+docker run -d --name watchtower \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower -i 300 open-webui
+sleep 2  # Waits 2 seconds.
+
+# Step 6: Check container status
+docker ps
